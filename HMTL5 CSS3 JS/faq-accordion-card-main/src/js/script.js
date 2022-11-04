a802e6c30abba9cae23015e45885a5e537d4c0f6
Agustin Sanchez Macal <agustin11089@gmail.com>
2022-11-04 12:11:54 -0600

    FAQ accordion solution
    initial commit of the working solution

---
+++ HMTL5 CSS3 JS/faq-accordion-card-main/src/js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    start();
+});
+
+function start() {
+    selection();
+}
+var i = false;
+function selection(){
+    const selected = document.querySelectorAll('.question');
+    selected.forEach(select => {
+        select.addEventListener('click', function(e) {
+            if(select.classList.contains('active')){
+                console.log(select.classList.contains('active'))
+                removeSelection();
+            } else {
+                removeSelection();
+                select.classList.add('active');
+                let image = select.children[0].children[1];
+                image.classList.add('up');
+                i = true;
+            }
+        })
+    })
+}
+
+function removeSelection(){
+    const options = document.querySelectorAll('.question');
+    options.forEach(option => {
+        option.classList.remove('active')
+        let image = option.children[0].children[1];
+        image.classList.remove('up');
+    })
+}
